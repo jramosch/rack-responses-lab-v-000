85669d15e9696cef00a14029b425ce7dea75c9d4
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-27 01:03:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,12 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write "Testing"
-    resp.finish
+
+    t = Time.new
+    if t.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
   end
 end
